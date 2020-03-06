4c23d359870800b165563e65a81a3ee773a59117
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-03-06 10:23:37 +0100

    Enable retry

--- src/main/kotlin/no/nav/syfo/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/syfo/config/ApplicationConfig.kt
@@ -4,10 +4,12 @@ import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.retry.annotation.EnableRetry
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.client.RestTemplate
 
 @Configuration
+@EnableRetry
 @EnableCaching
 @EnableScheduling
 class ApplicationConfig
