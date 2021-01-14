42d6d80549ceb62583f7d2afa65b6ed755bedd6e
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-14 09:38:01 +0100

    Refactor build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.github.jengelman.gradle.plugins.shadow.transformers.PropertiesFileTransformer
-import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.syfo"
@@ -9,7 +8,9 @@ version = "1.0.0"
 val javaxActivationVersion = "1.2.0"
 val jaxRiVersion = "2.3.2"
 val kotlinJacksonVersion = "2.11.2"
+val logstashVersion = "4.10"
 val prometheusVersion = "1.5.5"
+val slf4jVersion = "1.7.25"
 val tokenValidationSpringSupportVersion = "1.3.0"
 
 plugins {
@@ -34,24 +35,25 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-jersey")
-    implementation("org.springframework.boot:spring-boot-starter-logging")
-    implementation("org.springframework.boot:spring-boot-starter-cache")
-    implementation("org.springframework.boot:spring-boot-starter-jetty")
-    implementation("org.springframework.boot:spring-boot-starter-aop")
-
-    implementation("no.nav.security:token-validation-spring:$tokenValidationSpringSupportVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$kotlinJacksonVersion")
 
     implementation("com.sun.xml.ws:jaxws-ri:$jaxRiVersion")
     implementation("com.sun.activation:javax.activation:$javaxActivationVersion")
 
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-jersey")
+    implementation("org.springframework.boot:spring-boot-starter-jetty")
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("no.nav.security:token-validation-spring:$tokenValidationSpringSupportVersion")
+
     implementation("io.micrometer:micrometer-registry-prometheus:$prometheusVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$kotlinJacksonVersion")
-    implementation("net.logstash.logback:logstash-logback-encoder:4.10")
-    implementation("org.slf4j:slf4j-api:1.7.25")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("no.nav.security:token-validation-test-support:$tokenValidationSpringSupportVersion")
@@ -69,10 +71,6 @@ tasks {
     }
 
     withType<ShadowJar> {
-        transform(ServiceFileTransformer::class.java) {
-            setPath("META-INF/cxf")
-            include("bus-extensions.txt")
-        }
         transform(PropertiesFileTransformer::class.java) {
             paths = listOf("META-INF/spring.factories")
             mergeStrategy = "append"
