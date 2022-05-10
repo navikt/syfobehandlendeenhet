677943ee117016fb74c62eee36d92046621c967e
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2022-05-10 10:57:31 +0200

    Use distroless JDK17-image (#58)

--- src/main/kotlin/no/nav/syfo/App.kt
+++ src/main/kotlin/no/nav/syfo/App.kt
@@ -41,7 +41,7 @@ fun main() {
 
     applicationEngineEnvironment.monitor.subscribe(ApplicationStarted) { application ->
         applicationState.ready = true
-        application.environment.log.info("Application is ready")
+        application.environment.log.info("Application is ready, running Java VM ${Runtime.version()}")
     }
 
     val server = embeddedServer(
