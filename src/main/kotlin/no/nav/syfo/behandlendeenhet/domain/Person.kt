224d4d4906d5301c8e7da1e36219803557f0331c
Eirik Dahlen <37441744+eirikdahlen@users.noreply.github.com>
2022-10-04 12:04:26 +0200

    Change name of db-table (#70)
    Nav_Utland som navn virker litt rart, ettersom at innholdet i tabellen egentlig er en oversikt over personer og feltet is_nav_utland. Ved å kalle den `person` er vi mer rustet for en fremtidig utvidelse av tabellen også.

--- src/main/kotlin/no/nav/syfo/behandlendeenhet/domain/Person.kt
+++ src/main/kotlin/no/nav/syfo/behandlendeenhet/domain/Person.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.behandlendeenhet.domain
 import no.nav.syfo.domain.PersonIdentNumber
 import java.util.*
 
-data class NavUtland(
+data class Person(
     val uuid: UUID,
     val personident: PersonIdentNumber,
     val isNavUtland: Boolean
