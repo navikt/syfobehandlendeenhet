e7f928e0b45b25339246c8f1d3ffc45185dfa45e
Vetle Bu Solgård <vetle.bu.solgard@nav.no>
2024-02-26 13:31:43 +0100

    IS-1477: Add behandlingsnummer header to pdl requests (#128)

--- src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
@@ -4,18 +4,11 @@ import net.logstash.logback.argument.StructuredArgument
 import net.logstash.logback.argument.StructuredArguments
 import java.util.*
 
-const val TEMA_HEADER = "Tema"
-const val ALLE_TEMA_HEADERVERDI = "GEN"
-
 const val NAV_PERSONIDENT_HEADER = "nav-personident"
-
 const val NAV_PERSONIDENTER_HEADER = "Nav-Personidenter"
-
 const val NAV_CONSUMER_ID_HEADER = "Nav-Consumer-Id"
 const val NAV_APP_CONSUMER_ID = "syfobehandlendeenhet"
 const val NAV_CALL_ID_HEADER = "Nav-Call-Id"
-
-fun createCallId(): String = UUID.randomUUID().toString()
 
 fun callIdArgument(callId: String): StructuredArgument = StructuredArguments.keyValue("callId", callId)
 
