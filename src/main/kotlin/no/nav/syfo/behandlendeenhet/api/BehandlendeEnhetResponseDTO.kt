a8e4452a1a90e7090a3723fbb68010b7fc593913
geir-waagboe <geir.arne.waagbo@nav.no>
2025-05-12 16:14:23 +0200

    IS-3171: Inkluder tidspunkt og veilederident for oppfolgingsenhet

--- src/main/kotlin/no/nav/syfo/behandlendeenhet/api/BehandlendeEnhetResponseDTO.kt
+++ src/main/kotlin/no/nav/syfo/behandlendeenhet/api/BehandlendeEnhetResponseDTO.kt
@@ -5,15 +5,23 @@ import no.nav.syfo.behandlendeenhet.domain.BehandlendeEnhet
 import java.time.OffsetDateTime
 
 data class BehandlendeEnhetResponseDTO(
+    @Deprecated("Erstattet av geografisk enhet og oppfolgingsenhet")
+    val enhetId: String,
+    @Deprecated("Erstattet av geografisk enhet og oppfolgingsenhet")
+    val navn: String,
+
     val geografiskEnhet: Enhet,
     val oppfolgingsenhet: Enhet, // TODO: remove when not used anymore
     val oppfolgingsenhetDTO: OppfolgingsenhetDTO?,
 ) {
     companion object {
         fun fromBehandlendeEnhet(behandlendeEnhet: BehandlendeEnhet): BehandlendeEnhetResponseDTO {
+            val oppfolgingsenhet = behandlendeEnhet.oppfolgingsenhet?.enhet ?: behandlendeEnhet.geografiskEnhet
             return BehandlendeEnhetResponseDTO(
+                enhetId = oppfolgingsenhet.enhetId.value,
+                navn = oppfolgingsenhet.navn,
                 geografiskEnhet = behandlendeEnhet.geografiskEnhet,
-                oppfolgingsenhet = behandlendeEnhet.oppfolgingsenhet?.enhet ?: behandlendeEnhet.geografiskEnhet,
+                oppfolgingsenhet = oppfolgingsenhet,
                 oppfolgingsenhetDTO = behandlendeEnhet.oppfolgingsenhet?.enhet?.let {
                     OppfolgingsenhetDTO(
                         enhet = behandlendeEnhet.oppfolgingsenhet.enhet,
