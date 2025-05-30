8d26ad6766f2c40b5f61695e142d891a8fa55e50
Marius Syverstad Fossum <marius.syverstad.fossum@nav.no>
2025-05-30 08:40:27 +0200

    IS-3138: Erstattet enum for type med jsontype ved serialisering

--- src/main/kotlin/no/nav/syfo/behandlendeenhet/api/TildelHistorikkResponseDTO.kt
+++ src/main/kotlin/no/nav/syfo/behandlendeenhet/api/TildelHistorikkResponseDTO.kt
@@ -1,8 +1,8 @@
 package no.nav.syfo.behandlendeenhet.api
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonTypeName
 import no.nav.syfo.behandlendeenhet.EnhetService.Companion.SYSTEM_USER_IDENT
-import no.nav.syfo.behandlendeenhet.api.TildelOppfolgingsenhetHistorikkType.*
 import no.nav.syfo.behandlendeenhet.domain.Oppfolgingsenhet
 import java.time.LocalDateTime
 
@@ -10,17 +10,10 @@ data class TildelHistorikkResponseDTO(
     val oppfolgingsenheter: List<TildelOppfolgingsenhetHistorikkDTO>,
 )
 
-enum class TildelOppfolgingsenhetHistorikkType {
-    TILDELT_ANNEN_ENHET_AV_VEILEDER,
-    TILDELT_TILBAKE_TIL_GEOGRAFISK_ENHET_AV_VEILEDER,
-    TILDELT_TILBAKE_TIL_GEOGRAFISK_ENHET_AV_SYSTEM,
-}
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 sealed class TildelOppfolgingsenhetHistorikkDTO {
     abstract val createdAt: LocalDateTime
     abstract val veilederident: String
-    abstract val tildelOppfolgingsenhetHistorikkType: TildelOppfolgingsenhetHistorikkType
 
     companion object {
         fun fromOppfolgingsenhet(oppfolgingsenhet: Oppfolgingsenhet): TildelOppfolgingsenhetHistorikkDTO {
@@ -48,21 +41,21 @@ sealed class TildelOppfolgingsenhetHistorikkDTO {
     }
 }
 
+@JsonTypeName("TILDELT_ANNEN_ENHET_AV_VEILEDER")
 data class Tildelt(
     override val createdAt: LocalDateTime,
     override val veilederident: String,
-    override val tildelOppfolgingsenhetHistorikkType: TildelOppfolgingsenhetHistorikkType = TILDELT_ANNEN_ENHET_AV_VEILEDER,
     val enhet: EnhetDTO,
 ) : TildelOppfolgingsenhetHistorikkDTO()
 
+@JsonTypeName("TILDELT_TILBAKE_TIL_GEOGRAFISK_ENHET_AV_VEILEDER")
 data class TildeltTilbake(
     override val createdAt: LocalDateTime,
     override val veilederident: String,
-    override val tildelOppfolgingsenhetHistorikkType: TildelOppfolgingsenhetHistorikkType = TILDELT_TILBAKE_TIL_GEOGRAFISK_ENHET_AV_VEILEDER,
 ) : TildelOppfolgingsenhetHistorikkDTO()
 
+@JsonTypeName("TILDELT_TILBAKE_TIL_GEOGRAFISK_ENHET_AV_SYSTEM")
 data class TildeltTilbakeAvSystem(
     override val createdAt: LocalDateTime,
     override val veilederident: String,
-    override val tildelOppfolgingsenhetHistorikkType: TildelOppfolgingsenhetHistorikkType = TILDELT_TILBAKE_TIL_GEOGRAFISK_ENHET_AV_SYSTEM,
 ) : TildelOppfolgingsenhetHistorikkDTO()
