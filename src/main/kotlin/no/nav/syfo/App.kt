8c27cd12d3225d10ff4d16818fffbe50ce5996ed
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2026-05-19 13:28:31 +0200

    IS-3953: Forbedre shutdown-hook (#263)

--- src/main/kotlin/no/nav/syfo/App.kt
+++ src/main/kotlin/no/nav/syfo/App.kt
@@ -28,7 +28,6 @@ import no.nav.syfo.infrastructure.database.repository.EnhetRepository
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.slf4j.LoggerFactory
 import redis.clients.jedis.*
-import java.util.concurrent.TimeUnit
 
 const val applicationPort = 8080
 
@@ -163,7 +162,7 @@ fun main() {
 
     Runtime.getRuntime().addShutdownHook(
         Thread {
-            server.stop(10, 10, TimeUnit.SECONDS)
+            applicationState.ready = false
         }
     )
 
